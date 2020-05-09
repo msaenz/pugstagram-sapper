99c777c7d45a9d7ebd92dacdfd6ff8b416b9ea8d
Manuel Melgarejo Saenz <manuelmelgarejo@gmail.com>
2020-05-08 22:22:38 -0500

    commit inicial proyecto sapper

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="footer-copy">2020 PUGSTAGRAM PLATZI</div>
+  </div>
+</div>
